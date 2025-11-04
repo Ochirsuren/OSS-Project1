831794e93ca09936b1cf6621413e5a111116f6c8
DESKTOP-3K0QDOM\Dell <ochirsurenerdenee00526@gmail.com>
2025-11-04 11:14:20 +0900

    int main () {     int i = 0;     printf("First OSS Code\n");     printf("Second OSS Code\n")
        for(int i = 0; i < 10; i++){
            printf("Test 브랜치\n");
        }
        return 0;
    
    }

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main () {
-
     int i = 0;
-    printf("First OSS Code\n");
-    printf("second OSS Code\n")
-
-    for(int i = 0; i <  10; i++){
-        printf ("test 브랜치\n");
-    }
     
+    printf("First OSS Code\n");
+    printf("Second OSS Code\n")
 
+    for(int i = 0; i < 10; i++){
+        printf("Test 브랜치\n");
+    }
     return 0;
+
 }
